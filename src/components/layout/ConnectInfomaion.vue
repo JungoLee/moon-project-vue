5b423d8dd44d9878ea75124a4f14bf8c40a2776b
명훈PC\이명훈 <audgns722@gmail.com>
2025-03-29 23:06:08 +0900

    업데이트

---
+++ src/components/layout/ConnectInfomaion.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="connect-infomation-wrap">
+		<a href="tel:01032690202" target="_blank" title="새창열기">
+			<i class="ico-phone" aria-hidden="true"></i>
+		</a>
+		<a href="https://open.kakao.com/o/stA8hBah" target="_blank" title="새창열기">
+			<i class="ico-kakao" aria-hidden="true"></i>
+		</a>
+	</div>
+</template>
+
+<script>
+import logo from '@/assets/images/logo.png';
+
+export default {
+	props: ['index'], // 부모 컴포넌트로부터 index를 prop으로 받습니다.
+	data() {
+		return {
+			logo: logo,
+		};
+	},
+
+	methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.connect-infomation-wrap {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	@include rem(gap, 12);
+	.ico-phone {
+		display: inline-block;
+		@include rem(width, 60);
+		@include rem(height, 60);
+		@include rem(background-size, 60);
+		background-image: url(@/assets/images/ico_phone.png);
+	}
+	.ico-kakao {
+		display: inline-block;
+		@include rem(width, 60);
+		@include rem(height, 60);
+		@include rem(background-size, 60);
+		background-image: url(@/assets/images/ico_kakao.png);
+	}
+}
+</style>
--- src/pages/main/MainIndex.vue
+++ src/pages/main/MainIndex.vue
@@ -5,10 +5,12 @@
 		<HomeContainer v-show="currentIndex === 0" />
 		<!-- <AboutContainer v-show="currentIndex === 1" />
 		<WorkContainer v-show="currentIndex === 2" /> -->
+		<ConnectInfomaion></ConnectInfomaion>
 	</div>
 </template>
 
 <script>
+import ConnectInfomaion from '@/components/layout/ConnectInfomaion.vue';
 import MainUtillityMenu from '@/components/layout/MainUtillityMenu.vue';
 import HomeContainer from '@/components/pageLayout/HomeContainer.vue';
 // import AboutContainer from '@/components/pageLayout/AboutContainer.vue';
@@ -17,6 +19,7 @@ export default {
 	components: {
 		MainUtillityMenu,
 		HomeContainer,
+		ConnectInfomaion,
 		// AboutContainer,
 		// WorkContainer,
 	},
